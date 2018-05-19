f90008da0fc43abc83d5d5cb39da295f62d00287
dlujHub <dlujanschi2@gmail.com>
2018-05-19 13:52:43 +0300

    LaunchClass

---
+++ application/LaunchClass.java
@@ -0,0 +1,45 @@
+package application;
+
+import document.Document;
+
+import java.util.Random;
+
+
+public class LaunchClass {
+	
+	public String dictFile = "data/dict.txt";
+	
+	public LaunchClass() {
+		super();
+	}
+	
+	public Document getDocument(String text) {
+		return new document.BasicDocument(text);
+	}
+	
+	public textgen.MarkovTextGenerator getMTG() {
+		return new textgen.MarkovTextGeneratorLoL(new Random());
+	}
+	
+	public spelling.WordPath getWordPath() {
+		return new spelling.WPTree();
+	}
+	
+    public spelling.AutoComplete getAutoComplete() {
+        spelling.AutoCompleteDictionaryTrie tr = new spelling.AutoCompleteDictionaryTrie();
+        spelling.DictionaryLoader.loadDictionary(tr, dictFile);
+        return tr;
+    }
+    
+    public spelling.Dictionary getDictionary() {
+        spelling.Dictionary d = new spelling.DictionaryBST();
+        spelling.DictionaryLoader.loadDictionary(d, dictFile);
+    	return d;
+    }
+    
+    public spelling.SpellingSuggest getSpellingSuggest(spelling.Dictionary dic) {
+    	//return new spelling.SpellingSuggestNW(new spelling.NearbyWords(dic));
+    	return new spelling.NearbyWords(dic);
+    
+    }
+}
